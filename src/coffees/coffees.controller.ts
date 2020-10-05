63f4d7d9d46d14619b55c8b868f7cc66ca453d3a
Eduardo Almeida <contato.eduardopex@hotmail.com>
2020-10-05 09:06:02 -0300

    Status http

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
 
     @Get()
-    findAll() {
-        return 'This action returns all coffees';
+    findAll(@Res() response) {
+        response.status(200).send('This action returns all coffees');
     }
 
     @Get(':id')
@@ -14,5 +14,11 @@ export class CoffeesController {
     // }
     findOne( @Param('id') id: string) {
         return `This action returns #${id} coffees`;
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.GONE)
+    create(@Body() body){
+        return body;
     }
 }
