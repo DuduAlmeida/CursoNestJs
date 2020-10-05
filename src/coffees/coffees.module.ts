c0015fc10bc243d0ad68e4ca2b809d2ce6c4e624
Eduardo Almeida <contato.eduardopex@hotmail.com>
2020-10-05 11:42:16 -0300

    created DTO classes

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,7 +1,10 @@
-import { CoffeesController } from './coffees.controller';
 import { Module } from '@nestjs/common';
 
+import { CoffeesService } from './coffees.service';
+import { CoffeesController } from './coffees.controller';
+
 @Module({
-    
+    controllers: [CoffeesController],
+    providers: [CoffeesService]
 })
 export class CoffeesModule {}
