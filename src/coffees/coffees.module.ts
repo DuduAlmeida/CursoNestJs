6d59e1d72461263cc631c835b17c306895c0ba40
Eduardo Almeida <contato.eduardopex@hotmail.com>
2020-10-05 16:07:16 -0300

    updated coffee service

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,10 +1,14 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 
+import { Flavor } from './entities/flavor.entity';
+import { Coffee } from './entities/coffee.entity';
 import { CoffeesService } from './coffees.service';
 import { CoffeesController } from './coffees.controller';
 
 @Module({
     controllers: [CoffeesController],
-    providers: [CoffeesService]
+    providers: [CoffeesService],
+    imports: [TypeOrmModule.forFeature([Coffee, Flavor])]
 })
 export class CoffeesModule {}
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,25 @@
+import { Flavor } from './flavor.entity';
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()               //sql table === 'coffee'
+// @Entity('coffees')   //sql table === 'coffees'
 export class Coffee {
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     brand: string;
-    flavors: string[];
+
+    @JoinTable()
+    @ManyToMany(
+        type => Flavor,
+        (flavor) => flavor.coffees,
+        {
+            cascade: true,  // or optionally just insert or update ['insert']
+        }
+        )
+    flavors: Flavor[];
 }
