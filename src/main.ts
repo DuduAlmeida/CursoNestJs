aab9d18a57a66916f6370b91a815df31ed936d82
Eduardo Almeida <contato.eduardopex@hotmail.com>
2020-10-05 12:36:47 -0300

    endded API rest application

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true, // 👈
+      transform: true, // 👈
+      whitelist: true, // 👈
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
